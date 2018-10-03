938d93625167dc56897030e0de3a7fd3cc613152
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-03 20:47:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const drivers = ["Milo", "Otis", "Garfield"];
 drivers;
 const appendDriver =  ["Milo", "Otis", "Garfield"];
 appendDriver;
+
 function destructivelyAppendDriver()
 {
   drivers.push("Ralph");
